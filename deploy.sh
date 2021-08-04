5a0762b3a73b34e438275a75a936a2f690de3e1f
Jovany Vergara <diego.jobany11@gmail.com>
2021-08-03 19:05:16 -0500

    [main] cambio en deploy

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ echo brandonvergara.me >> CNAME
 git checkout master
 git add -A
 git commit -m "$comm"
-git push origin master --force
+git push origin main --force
 echo "8.- Actualizado repositorio, revisa cambios en el blog ----------------------------"
 echo "9.- Añadiendo commit a master principal ----------------------------"
 cd ..
@@ -28,4 +28,4 @@ git add -A
 git commit -m "$comm"
 git push origin main --force
 
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
